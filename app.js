d2908f9ba93387daf50a38a5a5a56b5f7db1ec4f
Michiyoshi Aikawa <aikawa@beavers.co.jp>
2023-10-28 22:33:57 +0900

    スクロールトップボタン追加

--- app.js
+++ app.js
@@ -4,4 +4,22 @@ $(function(){
         $(this).next('dd').toggleClass('none');
         $('span',this).toggleClass('rotate');
     });
+
+    // scroll to top
+    const topBtn = $('.back-to-top');
+    const scrollHeight = 100;
+    topBtn.hide();
+    $(window).scroll(function(){
+        if($(this).scrollTop() > scrollHeight){
+            topBtn.fadeIn();
+        }else{
+            topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function(){
+        $('body,html').animate({
+            scrollTop: 0
+        },800);
+        return false;
+    });
 });
